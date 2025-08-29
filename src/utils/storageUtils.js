790e6bd96302c433bf1ca5bea001096921c62cf8
Bavon <carlosbavon46@gmail.com>
2025-08-29 10:48:12 +0300

    Utils

--- src/utils/storageUtils.js
+++ src/utils/storageUtils.js
@@ -25,3 +25,19 @@ export const getFavorites = () =>
 
 export const getCart = () =>
   JSON.parse(localStorage.getItem("cart")) || [];
+
+export const removeFromFavorites = (dish) => {
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const updated = favorites.filter((item) => item.name !== dish.name);
+  localStorage.setItem("favorites", JSON.stringify(updated));
+};
+
+export const removeFromCart = (dish) => {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+  const updated = cart.filter((item) => item.name !== dish.name);
+  localStorage.setItem("cart", JSON.stringify(updated));
+};
+
+export const clearCart = () => {
+  localStorage.removeItem("cart");
+};
